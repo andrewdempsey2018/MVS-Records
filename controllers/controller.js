7f4638febd4070d1ec85e5a7725fb19908541a98
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-08-19 18:11:05 +0100

    Added add cart functionality

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,6 @@
+//create an instance of the cart model
+const Cart = require('../models/cart');
+
 const index = (req, res) => {
     res.render('index');
 }
@@ -10,8 +13,21 @@ const add = (req, res) => {
     res.render('add');
 }
 
+const add_cart = (req, res) => {
+    cart = new Cart(req.body);
+
+    cart.save()
+        .then((response) => {
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
 module.exports = {
     index,
     view,
     add,
+    add_cart,
 }
--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,6 @@ const controller = require('../controllers/controller');
 router.get('/', controller.index);
 router.get('/view', controller.view);
 router.get('/add', controller.add);
+router.post('/add_cart', controller.add_cart);
 
 module.exports = router;
