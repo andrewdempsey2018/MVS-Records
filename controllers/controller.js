b78428caaf4d4ff990965a9a69fb6620e83123dc
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-08-19 06:29:52 +0100

    Added "view" and "add" views

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,16 @@ const index = (req, res) => {
     res.render('index');
 }
 
+const view = (req, res) => {
+    res.render('view');
+}
+
+const add = (req, res) => {
+    res.render('iadd');
+}
+
 module.exports = {
     index,
+    view,
+    add,
 }
--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/controller');
 
 router.get('/', controller.index);
+router.get('/view', controller.view);
+router.get('/add', controller.view);
 
 module.exports = router;
